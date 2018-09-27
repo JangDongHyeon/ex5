c67059e52ebce25cfc4331dd08e269b6c5c939d3
USER <USER@USER18-PC>
2018-09-27 09:26:36 +0900

    2qjsWo

---
+++ BoardMVC/src/kr/itedu/exam10/test/Calc.java
@@ -0,0 +1,9 @@
+package kr.itedu.exam10.test;
+
+public class Calc {
+	public static int sum(int a,int b){
+	
+		return a+b;
+		
+	}
+}
---
+++ BoardMVC/src/kr/itedu/exam10/test/CalcTest.java
@@ -0,0 +1,14 @@
+package kr.itedu.exam10.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+	@Test
+	void test() {
+	assertEquals(3, Calc.sum(1, 2));
+	}
+
+}
